665db7506ed7412ba8e63fb839cbc50b43319cf2
nhistory <sehwan.lee.dev@gmail.com>
2022-02-24 00:37:08 -0400

    Error fix : not defined in es module scope

--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
 import express from 'express';
+// import path from 'path';
+// const __dirname = path.resolve();
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
-
+app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (req, res) => res.render('home'));
+
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 app.listen(3000, handleListen);
