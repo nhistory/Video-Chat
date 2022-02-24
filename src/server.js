2f37376017d6be3c846dcdfb7651925123408488
nhistory <sehwan.lee.dev@gmail.com>
2022-02-23 23:06:48 -0400

    Initial setup with babel, nodemon, pug, view

--- src/server.js
+++ src/server.js
@@ -1,1 +1,10 @@
-console.log('hello');
+import express from 'express';
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
+app.get('/', (req, res) => res.render('home'));
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+app.listen(3000, handleListen);
