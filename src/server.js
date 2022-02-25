15412cc4d4ba8d43d47ac27e7597899d820f59da
nhistory <sehwan.lee.dev@gmail.com>
2022-02-25 00:33:02 -0400

    Redirect in server and check app.js connection

--- src/server.js
+++ src/server.js
@@ -1,6 +1,4 @@
 import express from 'express';
-// import path from 'path';
-// const __dirname = path.resolve();
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
@@ -11,7 +9,8 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
-app.get('/', (req, res) => res.render('home'));
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.redirect('/'));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 app.listen(3000, handleListen);
