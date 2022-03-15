becb4abf240a51699d62ecaa966bb848a8bba869
nhistory <sehwan.lee.dev@gmail.com>
2022-03-15 01:03:13 -0300

    [#10-2] message connection test - success!!

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,18 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+// make eventListener with socket
+socket.addEventListener('open', () => {
+    console.log('Connected to Server');
+});
+
+socket.addEventListener('message', (message) => {
+    console.log('New message: ', message.data);
+});
+
+socket.addEventListener('close', () => {
+    console.log('Disconnected from server');
+});
+
+setTimeout(() => {
+    socket.send('hello from the browser');
+}, 10000);
