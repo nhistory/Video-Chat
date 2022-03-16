58e3281b99f0ff6345d52800576ce32f58a960f8
nhistory <sehwan.lee.dev@gmail.com>
2022-03-16 01:25:34 -0300

    [#11] connection between different browsers

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,5 @@
+const messageList = document.querySelector('ul');
+const messageForm = document.querySelector('form');
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 // make eventListener with socket
@@ -13,6 +15,10 @@ socket.addEventListener('close', () => {
     console.log('Disconnected from server');
 });
 
-setTimeout(() => {
-    socket.send('hello from the browser');
-}, 10000);
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector('input');
+    socket.send(input.value);
+    input.value = '';
+}
+messageForm.addEventListener('submit', handleSubmit);
--- src/server.js
+++ src/server.js
@@ -21,16 +21,26 @@ const server = http.createServer(app);
 // ws server with http
 const wss = new WebSocketServer({ server });
 
+// fake database
+const sockets = [];
+
 // make ws event
 wss.on('connection', (socket) => {
+    // push socket info into sockets array
+    sockets.push(socket);
+
     console.log('Connected to Browser ');
+
     socket.on('close', () => {
         console.log('Disconnected from the Browser');
     });
     socket.on('message', (message) => {
-        console.log(message.toString('utf8'));
+        // send message to all sockets in array
+        sockets.forEach((aSocket) => {
+            aSocket.send(message.toString());
+        });
+        // socket.send(message.toString());
     });
-    socket.send('hello!!!');
 });
 
 server.listen(3000, handleListen);
