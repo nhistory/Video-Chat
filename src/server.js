d3263c8c81bfd85174f5ce67bd19f8ee726a6a37
nhistory <nhistory@naver.com>
2022-03-19 21:04:43 -0300

    [#13] Make message input and room setup

--- src/server.js
+++ src/server.js
@@ -19,11 +19,9 @@ const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
 
 wsServer.on('connection', (socket) => {
-    socket.on('enter_room', (msg, done) => {
-        console.log(msg);
-        setTimeout(() => {
-            done();
-        }, 10000);
+    socket.on('enter_room', (roomName, done) => {
+        socket.join(roomName);
+        done();
     });
 });
 
